7ada6f96ad54a4494c78cf77ca6c72ab6032afeb
Johannes Pitz <johannes.pitz@tum.de>
2022-12-07 22:18:21 +0100

    Exercises

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,23 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    // let out = String::from(input);
+    // out.trim()
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let out = String::from(input);
+    out + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let out = String::from(input);
+    dbg!(out.replace("cars", "balloons"))
 }
 
 #[cfg(test)]
